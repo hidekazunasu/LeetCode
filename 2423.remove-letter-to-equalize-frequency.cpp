94af5adcadc8d7664207e4bd0cfadf6301b2ff21
hidekazunasu <hnasu@nscd.nstac.go.jp>
2023-03-29 20:12:46 +0900

    solved 2423

--- 2423.remove-letter-to-equalize-frequency.cpp
+++ 2423.remove-letter-to-equalize-frequency.cpp
@@ -5,11 +5,42 @@
  */
 
 // @lc code=start
-class Solution {
+#include <bits/stdc++.h>
+class Solution
+{
 public:
-    bool equalFrequency(string word) {
-        
+    bool equalFrequency(std::string word)
+    {
+        for (int i = 0; i < word.size(); ++i)
+        {
+            std::map<char, int> memo;
+            for (int j = 0; j < word.size(); ++j)
+            {
+
+                if (j == i)
+                {
+                    continue;
+                }
+                if (memo.count(word[j]))
+                {
+                    memo[word[j]]++;
+                }
+                else
+                {
+                    memo[word[j]] = 1;
+                }
+            }
+            std::set<int> st;
+            for (auto i : memo)
+            {
+                st.insert(i.second);
+            }
+            if (st.size() == 1)
+            {
+                return true;
+            }
+        }
+        return false;
     }
 };
 // @lc code=end
-
---
+++ 2591.distribute-money-to-maximum-children.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=2591 lang=cpp
+ *
+ * [2591] Distribute Money to Maximum Children
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    int distMoney(int money, int children)
+    {
+    }
+};
+// @lc code=end
